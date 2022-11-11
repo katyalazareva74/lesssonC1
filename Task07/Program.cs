3f72c89918e88dd4b639bcf733873855e130843e
Ekaterina Lazareva <lazarevakatya@mail.ru>
2022-11-11 17:08:27 +0300

    Task07 сделано

---
+++ Task07/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 07. Вводится трехзначное число N. Нужно вывести последнюю его цифру.
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 0)
+    num = -num;
+
+int last = num % 10;
+
+Console.WriteLine($"Последняя цифра будет {last}");
