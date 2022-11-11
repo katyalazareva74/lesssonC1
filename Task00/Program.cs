1d46b7675e18193b631119ac1f195982dc0a87c4
Ekaterina Lazareva <lazarevakatya@mail.ru>
2022-11-11 11:41:23 +0300

    Add Task00

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 00. Принмаем на вход число и умножаем его само на себя
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.WriteLine($"Квадрат числаs {number} = {square}");
