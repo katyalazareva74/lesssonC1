5d09c03f0492f0e6892c8ddf53446e4e20140c8c
Ekaterina Lazareva <lazarevakatya@mail.ru>
2022-11-11 18:17:10 +0300

    Task08 решено

---
+++ Task08/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 08. Принмаем на вход число. Нужно показать все четные числа от 1 до N.
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+
+if (N < 2)
+    Console.Write("Некорректная попытка ввода числа");    
+
+while (count <= N){
+        Console.Write(count + " ");
+        count +=2;
+}
