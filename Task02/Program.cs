b8140795961a6616c57b2af17ec92b361bfc5c39
Ekaterina Lazareva <lazarevakatya@mail.ru>
2022-11-11 19:33:39 +0300

    Форматирование кода  еще раз

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -9,9 +9,12 @@ int num2 = Convert.ToInt32(Console.ReadLine());
 if (num1 > num2)
     Console.WriteLine($"Большее число {num1}, Меньшее число {num2}");
 
-else if (num2 > num1)
-    Console.WriteLine($"Большее число {num2}, Меньшее число {num1}");
-
 else
-    Console.WriteLine("Числа равны");
+{
+    if (num2 > num1)
+        Console.WriteLine($"Большее число {num2}, Меньшее число {num1}");
+
+    else
+        Console.WriteLine("Числа равны");
+}
 
