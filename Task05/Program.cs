ddb6ec4a8ad7b76de70331c796d4bcd26f208a7e
Ekaterina Lazareva <lazarevakatya@mail.ru>
2022-11-11 16:48:05 +0300

    Task05 сделано

---
+++ Task05/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 03. Вводится число N. Нужно вывести все целые числа в интервале [-N, N].
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count;
+
+if (N < 0){
+    count = N;
+    N = -N;
+}
+else    
+    count = -N;    
+
+while (count <= N){
+    Console.Write(count + " ");
+    count++;
+}
