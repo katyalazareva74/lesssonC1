91468605236276006d83285a43a11e63e0f6d7b7
Ekaterina Lazareva <lazarevakatya@mail.ru>
2022-11-11 17:50:23 +0300

    Task06 решено

---
+++ Task06/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 06. Принмаем на вход число. Нужно определить является число четным или нет.
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+ if ((num % 2) == 0)
+    Console.Write("Число четное");   
+else 
+    Console.WriteLine("Число нечетное");    
