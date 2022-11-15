dd371e49abfafa8782eecfbd049804e6ce4c39b2
Ekaterina Lazareva <lazarevakatya@mail.ru>
2022-11-15 13:35:06 +0300

    Add Task011

---
+++ Task011/Program.cs
@@ -0,0 +1,11 @@
+﻿// На входе случайное трехзначное число. Нужно удалить вторую цифру из этого числа.
+int DeleteDigit(int num)
+{
+    int firstDigit = num / 100;
+    int lastDigit = num % 10;
+    return firstDigit * 10 + lastDigit;
+}
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+Console.WriteLine($"Двухзначное число --> {DeleteDigit(number)}");
