5fe8261412e1c8278b19fcc2e1632a22b7db33fc
Ekaterina Lazareva <lazarevakatya@mail.ru>
2022-11-15 14:47:14 +0300

    Add Task012

---
+++ Task012/Program.cs
@@ -0,0 +1,19 @@
+﻿// Вводятся два числа. Нужно узнать кратно ли первое число второму. Если нет, то вывести остаток от деления.
+Console.Write("Введите первое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(Multiplicity(number1, number2) ? "Да" : $"Нет, остаток от деления --> {number1 % number2}");
+
+Console.WriteLine(Multiplicity1(number1, number2) == 0 ? "Да" : $"Нет, остаток от деления --> {Multiplicity1(number1, number2)}");
+
+// bool Multiplicity(int num1, int num2)
+//{
+  //  return num1 % num2 == 0;
+//}
+
+int Multiplicity1(int num1, int num2)
+{
+    return num1 % num2;
+}
