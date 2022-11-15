ad94ce607dbd04da3b8de715f8c32778c9efbc88
Ekaterina Lazareva <lazarevakatya@mail.ru>
2022-11-15 17:18:45 +0300

    Add Task013

---
+++ Trak013/Program.cs
@@ -0,0 +1,30 @@
+﻿// Вводится число. Нужно вывести 3 цифру этого числа или сообщить, что третьей цифры нет.
+
+Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int threeDigit = ThreeDigit(number);
+
+if (threeDigit == 0)
+    Console.WriteLine($"{number} --> Нет третьей цифры");
+else
+    Console.WriteLine($"{number} --> {ThirdDigit(threeDigit)}");
+
+int ThreeDigit(int num)
+{
+    if (num < 100)
+        return 0;
+    else
+    {
+        while (num >= 1000)
+        {
+            num = num / 10;
+        }
+        return num;
+    }
+}
+
+int ThirdDigit(int num)
+{
+    return num % 10;
+}
