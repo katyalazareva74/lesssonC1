13a03d2d5de1e42989caa78648ff303384141cab
Ekaterina Lazareva <lazarevakatya@mail.ru>
2022-11-15 15:02:18 +0300

    Add Task014

---
+++ Task014/Program.cs
@@ -0,0 +1,8 @@
+﻿// Вводится число. Нужно узнать число кратно 7 и 23 или нет
+Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Multiplicity2 (number)? "Да" : "Нет");
+
+bool Multiplicity2 (int num){
+    return num % 7 == 0 && num % 23 == 0;
+}
