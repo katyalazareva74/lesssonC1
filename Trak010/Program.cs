0ad29f4d7423e793de7d27c441dc0f310a722265
Ekaterina Lazareva <lazarevakatya@mail.ru>
2022-11-15 16:19:46 +0300

    Add Task010

---
+++ Trak010/Program.cs
@@ -0,0 +1,11 @@
+﻿// Вводится трёхзначное число. Нужно показать вторую цифру этого числа. 
+
+Console.Write("Введите трехзначное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Вторая цифра --> {SecondDigit(number)}");
+
+int SecondDigit(int num)
+{
+    int secondDigit = num / 10;
+    return secondDigit % 10;
+}
