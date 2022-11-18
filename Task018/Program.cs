74d07bd322e149d991ad482a6183fa7a4147f4f1
Ekaterina Lazareva <lazarevakatya@mail.ru>
2022-11-18 12:32:27 +0300

    Add Task018

--- Task018/Program.cs
+++ Task018/Program.cs
@@ -1,19 +1,18 @@
 ﻿// Вводятся номер четверти. Нужно определить диапазон точек в этой четверти. X!=0, Y!=0.
-Console.WriteLine("Введите координаты точки ");
+Console.WriteLine("Введите номер четверти ");
+string quarter = Console.ReadLine();
 
-int num = Convert.ToInt32(Console.ReadLine());
-
-string Quarter(int num1)
+string Quarter(string num)
 {
-    if (num == 1)
+    if (num == "1")
         return "x > 0 && y > 0";
-    if (num == 2)
+    if (num == "2")
         return "x < 0 && y > 0";
-    if (num == 3)
+    if (num == "3")
         return "x < 0 && y < 0";
-    if (num == 4)
+    if (num == "4")
         return "x > 0 && y < 0";
     return "Ввели некорректное число";
 }
 
-Console.WriteLine(Quarter(num));
+Console.WriteLine(Quarter(quarter));
