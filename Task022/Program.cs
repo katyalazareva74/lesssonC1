e1681f113da6b0343d55f755fc1939882e35e6e5
Ekaterina Lazareva <lazarevakatya@mail.ru>
2022-11-18 12:28:14 +0300

    Add Task022

---
+++ Task022/Program.cs
@@ -0,0 +1,14 @@
+﻿// Вводтся число N. Нужно вывести в виде таблицы квадраты чисел от 1 до N.
+Console.WriteLine("Введите  число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+void Squar(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($"{count} --> {count * count}");
+        count++;
+    }
+}
+Squar(num1);
