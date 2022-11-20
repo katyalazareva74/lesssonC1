2ffe7a94df99a87c93196f4f2bff57bd87ecc591
Ekaterina Lazareva <lazarevakatya@mail.ru>
2022-11-20 20:24:01 +0300

    Add Task023

---
+++ Task023/Program.cs
@@ -0,0 +1,22 @@
+﻿// Вводтся число N. Нужно вывести в виде таблицы кубы чисел от 1 до N.
+Console.Write("Введите  число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+if (num1 == 0)
+    Console.WriteLine("Некорректный ввод числа");
+else
+    Cube(num1);
+
+void Cube(int num)
+{
+    int count = 1;
+    if (num < 0)
+    {
+        count = num;
+        num = -1;
+    }
+    while (count <= num)
+    {
+        Console.WriteLine($"{count} --> {count * count * count}");
+        count++;
+    }
+}
