9848f06dd8f795c301aef9b00d5b0edcf11f8c8b
Ekaterina Lazareva <lazarevakatya@mail.ru>
2022-11-22 12:23:56 +0300

    Add Task028

---
+++ Task028/Program.cs
@@ -0,0 +1,20 @@
+﻿// Вводится число А. Нужно узнать произведение чисел от 1 до А.
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0)
+{
+    int prodDigit = ProdDigits(number);
+    Console.WriteLine($"{number} --> {prodDigit}");
+}
+else
+    Console.WriteLine("Введно некорректное число");
+
+int ProdDigits(int num)
+{
+    int prod = 1; 
+    for (int i = 1; i <= num; i++)
+    {
+        prod = prod * i;
+    }
+    return prod;
+}
