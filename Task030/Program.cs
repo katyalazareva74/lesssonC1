8cfd46c9d0700be514493677370374e3db0d803e
Ekaterina Lazareva <lazarevakatya@mail.ru>
2022-11-22 13:22:31 +0300

    Add Task030

---
+++ Task030/Program.cs
@@ -0,0 +1,25 @@
+﻿// Вывести массив, заполненный 0 и 1.
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+FillArray(array);
+PrintArray(array);
+void FillArray(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(0, 2);
+    }
+}
+void PrintArray(int[] arr)
+{
+    Console.Write("{ ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i == arr.Length - 1)
+            Console.Write($"{arr[i]} }}");
+        else
+            Console.Write($"{arr[i]}, ");
+    }
+}
