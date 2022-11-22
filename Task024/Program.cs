1989533b10dbc6867a0f277d2c4305a85fa39bb1
Ekaterina Lazareva <lazarevakatya@mail.ru>
2022-11-22 10:57:05 +0300

    Add Task024

---
+++ Task024/Program.cs
@@ -0,0 +1,21 @@
+﻿// Вводится число А. Нужно узнать сумму числе от 1 до А.
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0)
+{
+    int sumDigit = SumDigits(number);
+    //Console.WriteLine($"{number} --> {SumDigits(number)}");
+    Console.WriteLine($"{number} --> {sumDigit}");
+}
+else
+    Console.WriteLine("Введно некорректное число");
+
+int SumDigits(int num)
+{
+    int sum = default; // неопределенное значение
+    for (int i = 1; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
