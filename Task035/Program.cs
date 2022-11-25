d8f4327934e60f6e9e2018c7edb985d7291c1ead
Ekaterina Lazareva <lazarevakatya@mail.ru>
2022-11-25 20:28:52 +0300

    Add Task035

---
+++ Task035/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задагн массив из 123 элементов. Нужно узнать сколько двузначных элементов в массиве.
+int[] array = CreateArrayRndInt(10, -10, 200);
+PrintArray(array);
+int number = FindNumber(array);
+Console.WriteLine(number);
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size]; 
+    Random rnd = new Random(); 
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+int FindNumber(int[] arr)
+{int j=0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+       if (arr[i] > 10 & arr[i]<99)
+        j++;
+     
+    }
+    return j; 
+}
+void PrintArray(int[] arr)
+{
+    Console.Write("{ ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+            Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine("}");
+}
