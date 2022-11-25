cf8c711bb305df3a1074cf9fe84ff109c684ba20
Ekaterina Lazareva <lazarevakatya@mail.ru>
2022-11-25 20:34:09 +0300

    Add Task033

---
+++ Task033/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задан массив. Присутствует ли в массиве заданное число.
+int[] array = CreateArrayRndInt(10, -10, 10);
+PrintArray(array);
+Console.WriteLine(FindNumber(array, 4) ? "yes" : "no");
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size]; 
+    Random rnd = new Random(); 
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+bool FindNumber(int[] arr, int num)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+       if (arr[i] == num)
+        return true;
+     
+    }
+    return false; 
+}
+void PrintArray(int[] arr)
+{
+    Console.Write("{ ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+            Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine("}");
+}
