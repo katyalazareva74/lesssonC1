ae8f7b66fec341634d17a31550cf267176a3e494
Ekaterina Lazareva <lazarevakatya@mail.ru>
2022-11-25 20:35:18 +0300

    Add Task032

---
+++ Task032/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задан массив. Нужно отрицательные значения элементов массива переделать в положительные и вывести на экран.
+int[] array = CreateArrayRndInt(5, -9, 10);
+PrintArray(array);
+NegativeArray(array);
+PrintArray(array);
+void NegativeArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = -arr[i];
+    }
+}
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size]; 
+    Random rnd = new Random(); 
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+void PrintArray(int[] arr)
+{
+    Console.Write("{ ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+            Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine("}");
+}
