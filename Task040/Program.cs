577b4e45ddbaef787d428d2c43e16acebc53a9ea
Ekaterina Lazareva <lazarevakatya@mail.ru>
2022-11-29 12:44:48 +0300

    Add Task040

---
+++ Task040/Program.cs
@@ -0,0 +1,12 @@
+﻿// Вводится три часла - это длины сторон треугольника. 
+//Нужно узнать существует ли такой треугольник. Треукгольник существует, если каждая из сторон меньше суммы двух других сторон.
+Console.WriteLine("Введите  три числа: ");
+int a1 = Convert.ToInt32(Console.ReadLine());
+int b1 = Convert.ToInt32(Console.ReadLine());
+int c1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Traiangl(a1, b1, c1) ? "Да" : "Нет");
+
+bool Traiangl(int a, int b, int c)
+{
+    return a < b + c && b < a + c && c < a + b;
+}
