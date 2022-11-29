41b32a666ef8575ad4456bff13de41f823ff678a
Ekaterina Lazareva <lazarevakatya@mail.ru>
2022-11-29 12:22:42 +0300

    Add Task039

---
+++ Task039/Program.cs
@@ -0,0 +1,35 @@
+﻿// Перевернуть одномергый массив: {1, 2, 3, 4, 5}-->{5, 4, 3, 2, 1}.
+int[] array = CreateArrayRndInt(7, 10, 50);
+PrintArray(array);
+ReversArray(array);
+PrintArray(array);
+void ReversArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length / 2; i++)
+    {
+        int temp = arr[i];
+        arr[i] = arr[arr.Length - 1 - i];
+        arr[arr.Length - 1 - i] = temp;
+    }
+}
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+void PrintArray(int[] arr)
+{
+    Console.Write("{ ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+            Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine("}");
+}
