1af6bc1644963d08b946415894d49d4f667ec0cf
Ekaterina Lazareva <lazarevakatya@mail.ru>
2022-11-30 11:57:02 +0300

    Changes Task041

--- Task041/Program.cs
+++ Task041/Program.cs
@@ -3,33 +3,46 @@
 //-1, -7, 567, 89, 223-> 3
 Console.Write("Сколько чисел будет вводиться: ");
 int number = Convert.ToInt32(Console.ReadLine());
+number = CheckNumber(number);
 int[] array = new int[number];
 //I ВАРИАНТ (ВВОД В СТОЛБИК)
-array = EnteringNumbers(number);
+//array = EnteringNumbers(number);
 //II ВАРИАНТ (ВВОД ОДНОЙ СТРОКОЙ)
 array = EnteringNumbersStr(number);
 int numPositiv = NumberPositiv(array);
 PrintArray(array);
 Console.WriteLine($"--> {numPositiv}");
 
-int[] EnteringNumbersStr(int n)
+string[] Entering(int n)
 {
     string text = String.Empty;
     Console.WriteLine("Введите числа через пробел в одну строку: ");
     text = Console.ReadLine()!;
-    string[] arrStr = text!.Split(' ');
-    int[] arr = new int[n];
-    for (int i = 0; i < n; i++)
+    string[] arrStr1 = text!.Split(' ');
+    while (arrStr1.Length < n)
+    {
+        Console.WriteLine("Введите числа через пробел в одну строку: ");
+        text = String.Empty;
+        text = Console.ReadLine()!;
+        arrStr1 = text!.Split(' ');
+    }
+    return arrStr1;
+}
+int[] EnteringNumbersStr(int size)
+{
+    int[] arr = new int[size];
+    string[] arrStr = Entering(size);
+    for (int i = 0; i < size; i++)
     {
         arr[i] = Convert.ToInt32(arrStr[i]);
     }
     return arr;
 }
-int[] EnteringNumbers(int n)
+int[] EnteringNumbers(int size)
 {
-    int[] arr = new int[n];
+    int[] arr = new int[size];
     Console.WriteLine("Введите числа: ");
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < size; i++)
     {
         Console.Write($"{i + 1} число: ");
         arr[i] = Convert.ToInt32(Console.ReadLine());
@@ -55,4 +68,14 @@ void PrintArray(int[] arr)
         else Console.Write($"{arr[i]} ");
     }
     Console.Write("} ");
+}
+int CheckNumber(int num)
+{
+    while (num < 1)
+    {
+        Console.WriteLine("Некорректный ввод числа!");
+        Console.Write("Сколько чисел будет вводиться: ");
+        num = Convert.ToInt32(Console.ReadLine());
+    }
+    return num;
 }
