6a9744ba5ce9603806f802a2348cbaa9e567ab50
Ekaterina Lazareva <lazarevakatya@mail.ru>
2022-12-02 11:00:39 +0300

    Add Task046

---
+++ Task046/Program.cs
@@ -0,0 +1,25 @@
+﻿// Создание и печать двумерного массива.
+
+int[,] array2D = CreateMatrixRndInt(3, 4, -50, 50);
+PrintMatrix(array2D);
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = rnd.Next(min, max + 1);
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j],4} ");
+        Console.WriteLine();
+    }
+}
