73e369ab836c76a3e1bf55977bfa344b8effc24e
Ekaterina Lazareva <lazarevakatya@mail.ru>
2022-12-06 14:42:13 +0300

    Add Task053

---
+++ Task053/Program.cs
@@ -0,0 +1,36 @@
+﻿// В двумерном массиве поменять местами первую и последнюю строку.
+int[,] matrix1 = CreateMatrixRndInt(4, 4, 0, 10);
+PrintMatrix(matrix1);
+ReplaceRows(matrix1);
+Console.WriteLine();
+PrintMatrix(matrix1);
+
+void ReplaceRows(int[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        int temp = matrix[0, j];
+        matrix[0, j] = matrix[matrix.GetLength(0) - 1, j];
+        matrix[matrix.GetLength(0) - 1, j] = temp;
+    }
+}
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = rnd.Next(min, max + 1);
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j],4} ");
+        Console.WriteLine();
+    }
+}
