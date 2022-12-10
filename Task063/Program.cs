b2efded4b9d215bab90e34134ef81ba4f20815b1
Ekaterina Lazareva <lazarevakatya@mail.ru>
2022-12-10 12:00:42 +0300

    Add Task063

---
+++ Task063/Program.cs
@@ -0,0 +1,12 @@
+﻿// Вывести все числа от 1 до N.
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+NaturalNumbers(number);
+
+void NaturalNumbers(int num)
+{
+    if (num == 0) return;
+    NaturalNumbers(num - 1);
+    Console.Write($"{num} ");
+}
+
