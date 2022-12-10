89dcbdd12d0fd0cdc674ab04d2ebdc326071002e
Ekaterina Lazareva <lazarevakatya@mail.ru>
2022-12-10 12:10:31 +0300

    Add Task067

---
+++ Task067/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти сумму цифр в числе N.
+Console.WriteLine("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int sum = NaturalNumbers(number1);
+Console.WriteLine(sum);
+
+int NaturalNumbers(int num1)
+{
+    return num1 % 10 == 0 ? 0 : num1 % 10 + NaturalNumbers(num1 / 10);
+}
