42c81f66b51b3d8f278d4639ac96d091e0f68705
Ekaterina Lazareva <lazarevakatya@mail.ru>
2022-12-11 16:40:59 +0300

    Add Task069

---
+++ Task069/Program.cs
@@ -0,0 +1,11 @@
+﻿// Возведение числа А в степень B.
+Console.WriteLine("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень числа: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int sum = Exponentiation(number1, number2);
+Console.WriteLine(sum);
+int Exponentiation(int num, int degree)
+{
+    return degree == 0 ? 1 : num * Exponentiation(num, degree - 1);
+}
